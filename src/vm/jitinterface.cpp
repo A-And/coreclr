0601729d826c02e191a6305d28b7a0a15ae8f4ea
Jan Vorlicek <janvorli@microsoft.com>
2018-10-05 03:08:51 +0200

    Remove context statics stuff (#20256)
    * Remove context statics stuff part 1
    
    This change removes all context statics stuff from the runtime since
    context statics are not supported and this code was obsolete.
    
    * Remove context statics stuff from the debugger code

--- src/vm/jitinterface.cpp
+++ src/vm/jitinterface.cpp
@@ -1562,13 +1562,6 @@ void CEEInfo::getFieldInfo (CORINFO_RESOLVED_TOKEN * pResolvedToken,
                 fieldFlags |= CORINFO_FLG_FIELD_INITCLASS;
         }
         else
-        if (pField->IsContextStatic())
-        {
-            fieldAccessor = CORINFO_FIELD_STATIC_ADDR_HELPER;
-
-            pResult->helper = CORINFO_HELP_GETSTATICFIELDADDR_CONTEXT;
-        }
-        else
         {
             // Regular or thread static
             CORINFO_FIELD_ACCESSOR intrinsicAccessor;
@@ -1620,7 +1613,6 @@ void CEEInfo::getFieldInfo (CORINFO_RESOLVED_TOKEN * pResolvedToken,
         //
         if ((flags & CORINFO_ACCESS_ADDRESS) &&
             !pField->IsThreadStatic() &&
-            !pField->IsContextStatic() &&
             (fieldAccessor != CORINFO_FIELD_STATIC_TLS))
         {
             fieldFlags |= CORINFO_FLG_FIELD_SAFESTATIC_BYREF_RETURN;
