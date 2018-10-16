486efb60fefe03a79a8b5adae22b3a04e6b1f9d2
Simon Nattress <nattress@gmail.com>
2018-10-16 11:07:25 -0700

    High entropy ASLR for native images
    Enable high entropy for 64bit native images, which expands the set of virtual address bases a native image can be loaded at.

--- src/zap/zapimage.cpp
+++ src/zap/zapimage.cpp
@@ -1536,6 +1536,10 @@ void ZapImage::OutputTables()
 #endif // _DEBUG
         {
             dllCharacteristics |= IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE;
+#ifdef _TARGET_64BIT_
+            // Large address aware, required for High Entry VA, is always enabled for 64bit native images.
+            dllCharacteristics |= IMAGE_DLLCHARACTERISTICS_HIGH_ENTROPY_VA;
+#endif
         }
 
         SetDllCharacteristics(dllCharacteristics);
